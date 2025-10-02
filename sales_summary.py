157661e195113a4dc24d454f140494eeffa5287d
pratikkabra143 <pratikkabra2000@gmail.com>
2025-10-02 16:19:15 +0530

    Saving Chart to outputs

--- sales_summary.py
+++ sales_summary.py
@@ -1,6 +1,7 @@
 import sqlite3
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 conn = sqlite3.connect("data/sales_data.db")
 
@@ -22,3 +23,10 @@ plt.ylabel("Revenue")
 plt.xticks(rotation=45)
 plt.tight_layout()
 plt.show()
+
+output_dir = "outputs"
+os.makedirs(output_dir, exist_ok=True)
+
+plt.savefig("outputs/sales_chart.png")
+
+conn.close()
